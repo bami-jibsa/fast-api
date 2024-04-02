6b9fe8255e20f880301187e60e267a766d3530c7
bami-jibsa <tlswjddn0615@naver.com>
2024-04-03 04:24:17 +0900

    projects

---
+++ projects/myapi/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+from domain.question import question_router
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 모든 출처에서 오는 요청을 허용하도록 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
+
+
+app.include_router(question_router.router)
